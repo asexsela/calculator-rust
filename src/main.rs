8c568505a18b4269a0baee00c71420e1dae41b08
Aleksandr Seliukov <asexsela@gmail.com>
2022-11-20 11:59:28 +0100

    first

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::env::{args, Args};
+
+fn main() {
+    let mut args: Args = args();
+
+    let first: String = args.nth(1).unwrap();
+    let operator: char = args.nth(0).unwrap().chars().next().unwrap();
+    let second: String = args.nth(0).unwrap();
+
+    let first_number: f32 = first.parse::<f32>().unwrap();
+    let second_number: f32 = second.parse::<f32>().unwrap();
+    let result: f32 = operate(operator, first_number, second_number);
+
+    println!("{}", output(first_number, operator, second_number, result))
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    match operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '/' => first_number / second_number,
+        '*'|'x'|'X' => first_number * second_number,
+        _ => panic!("Invalid operator")
+    }
+}
+
+fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+    return format!(
+        "{} {} {} = {}",
+        first_number, operator, second_number, result
+    );
+}
